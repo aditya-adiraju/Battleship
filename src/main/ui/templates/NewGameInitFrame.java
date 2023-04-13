7a7411b20db57f0ecf2d68a7b3521de88ff7633b
aditya-adiraju <adadiraju@cisb.org.in>
2023-04-12 18:51:16 -0700

    Phase 4: Final commit (GRADE THIS)

--- src/main/ui/templates/NewGameInitFrame.java
+++ src/main/ui/templates/NewGameInitFrame.java
@@ -107,11 +107,17 @@ public class NewGameInitFrame extends JFrame implements ActionListener, WindowLi
         }
     }
 
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS: does nothing
     @Override
     public void windowOpened(WindowEvent e) {
 
     }
 
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS: prints event log
     @Override
     public void windowClosing(WindowEvent e) {
         EventLog log = EventLog.getInstance();
@@ -120,26 +126,41 @@ public class NewGameInitFrame extends JFrame implements ActionListener, WindowLi
         }
     }
 
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS: does nothing
     @Override
     public void windowClosed(WindowEvent e) {
 
     }
 
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS: does nothing
     @Override
     public void windowIconified(WindowEvent e) {
 
     }
 
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS: does nothing
     @Override
     public void windowDeiconified(WindowEvent e) {
 
     }
 
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS: does nothing
     @Override
     public void windowActivated(WindowEvent e) {
 
     }
 
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS: does nothing
     @Override
     public void windowDeactivated(WindowEvent e) {
 
